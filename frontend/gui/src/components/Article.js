0cb032e56a52617565516b4db3d8ef9de64a7d65
lcmencia <luiscarlos@lcmencia.com>
2020-04-09 18:51:06 -0400

    Handing Post Requests

--- frontend/gui/src/components/Article.js
+++ frontend/gui/src/components/Article.js
@@ -1,58 +1,56 @@
-import React from 'react';
-import { List, Avatar } from 'antd';
-import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+import React from "react";
+import { List, Avatar, Icon } from "antd";
 
+const IconText = ({ type, text }) => (
+  <span>
+    <Icon
+      type={type}
+      style={{
+        marginRight: 8
+      }}
+    />
+    {text}
+  </span>
+);
 
-const IconText = ({ icon, text }) => (
-    <span>
-      {React.createElement(icon, { style: { marginRight: 8 } })}
-      {text}
-    </span>
-  );
-
-const Articles = (props) => {
-    return (
-        <List
-    itemLayout="vertical"
-    size="large"
-    pagination={{
-      onChange: page => {
-        console.log(page);
-      },
-      pageSize: 3,
-    }}
-    dataSource={props.data}
-    footer={
-      <div>
-        <b>ant design</b> footer part
-      </div>
-    }
-    renderItem={item => (
-      <List.Item
-        key={item.title}
-        actions={[
-          <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-          <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-          <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-        ]}
-        extra={
-          <img
-            width={272}
-            alt="logo"
-            src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+const Articles = props => {
+  return (
+    <List
+      itemLayout="vertical"
+      size="large"
+      pagination={{
+        onChange: page => {
+          console.log(page);
+        },
+        pageSize: 3
+      }}
+      dataSource={props.data}
+      renderItem={item => (
+        <List.Item
+          key={item.title}
+          actions={[
+            <IconText type="star-o" text="156" />,
+            <IconText type="like-o" text="156" />,
+            <IconText type="message" text="2" />
+          ]}
+          extra={
+            <img
+              width={272}
+              alt="logo"
+              src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+            />
+          }
+        >
+          <List.Item.Meta
+            avatar={<Avatar src={item.avatar} />}
+            title={<a href={`/articles/${item.id}`}> {item.title} </a>}
+            description={item.description}
           />
-        }
-      >
-        <List.Item.Meta
-          avatar={<Avatar src={item.avatar} />}
-          title={<a href={`/${item.id}`}>{item.title}</a>}
-          description={item.description}
-        />
-        {item.content}
-      </List.Item>
-    )}
-  />
-    )
-}
+          {item.content}
+        </List.Item>
+      )}
+    />
+  );
+};
 
 export default Articles;
--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import ArticleList from './containers/ArticleListView';
-import ArticleDetail from './containers/ArticleDetailView';  
+import ArticleDetail from './containers/ArticleDetailView';
 
 const BaseRouter = () => (
     <div>
-        <Route exact path='/' component={ArticleList}/>
-        <Route exact path='/:articleID' component={ArticleDetail}/>
+        <Route exact path='/' component={ArticleList} />
+        <Route exact path='/:articleID' component={ArticleDetail} />
+        <Route exact path="/articles/:articleID/" component={ArticleDetail} />
     </div>
 );
 
