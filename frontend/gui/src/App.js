f2c9ad61ffb9852ad28308db2bb96752662d3f90
lcmencia <luiscarlos@lcmencia.com>
2020-04-12 22:44:13 -0400

    Authenticate Users with React and Django

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,18 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { connect } from 'react-redux';
 import BaseRouter from './routes';
-import './App.css';
 import 'antd/dist/antd.css';
+import * as actions from './store/actions/auth';
 
 import CustomLayout from './containers/Layout';
 
 class App extends Component {
+
+    componentDidMount() {
+        this.props.onTryAutoSignup();
+    }
+
     render() {
         return (
             <div className="App">
                 <Router>
-                    <CustomLayout>
-                        <BaseRouter/>
+                    <CustomLayout {...this.props}>
+                        <BaseRouter />
                     </CustomLayout>
                 </Router>
             </div>
@@ -20,4 +26,16 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.token != null
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onTryAutoSignup: () => dispatch(actions.authCheckState())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
